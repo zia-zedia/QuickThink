95a8e15d391e01e2fdfbc5623661d21f926dbe29
zia-zedia <ziagallaf578@gmail.com>
2023-11-16 17:11:27 +0300

    worked on frontend stuff

--- quickthink/src/pages/test/[id].tsx
+++ quickthink/src/pages/test/[id].tsx
@@ -1,9 +1,43 @@
 import { useRouter } from "next/router";
 import { api } from "~/utils/api";
-import { TestType } from "~/drizzle/schema";
-import { useEffect, useState } from "react";
+import { Question, TestType } from "~/drizzle/schema";
+import {
+  ReactNode,
+  useEffect,
+  useState,
+  createContext,
+  useContext,
+} from "react";
+import { session } from "~/server/auth/auth";
 
-export default function TestPage() {
+export function TestPageLayout(props: { children: ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col items-center bg-[#EDF0FF]">
+      {props.children}
+    </div>
+  );
+}
+
+type TextContextType = {
+  testStarted: boolean;
+  setTestStarted: (isStarted: boolean) => void;
+};
+export const TestContext = createContext<TextContextType>({
+  testStarted: false,
+  setTestStarted: () => {
+    return;
+  },
+});
+
+export default function TestPageContainer() {
+  return (
+    <TestPageLayout>
+      <TestPage />
+    </TestPageLayout>
+  );
+}
+
+export function TestPage() {
   const router = useRouter();
   const test_id = Array.isArray(router.query.id)
     ? router.query.id[0]
@@ -13,120 +47,172 @@ export default function TestPage() {
     return;
   }
 
+  const sessionId = localStorage.getItem("session_id");
   const sessionStarter = api.tests.startSession.useMutation({
     onSuccess: (value) => {
-      const sessionId = value[0]?.sessionId;
-      if (sessionId === undefined) {
-        return;
-      }
-      localStorage.setItem("session_id", sessionId);
-      console.log(localStorage.getItem("session_id"));
+      setTestStarted(true);
+      localStorage.setItem("session_id", value[0]?.sessionId!);
     },
   });
-  const timer = api.tests.checkSession.useMutation({
-    onSuccess: (value) => {
-      console.log(value[0]?.startTime);
-    },
-    onError: (value) => {
-      if (value.data?.code === "NOT_FOUND") {
-        sessionStarter.mutate({ test_id: test_id });
-      }
-    },
-  });
-  const testData = api.tests.getTestIntroWithId.useMutation({
-    onSuccess: (value) => {
-      console.log(value?.testData);
-    },
-    onError: (value) => {
-      if (value.data?.code === "BAD_REQUEST") {
-        return (
-          <>
-            <h1>An error happened</h1>,
-            <p>make sure you entered a valid test id.</p>
-            <p>{value.message}</p>
-          </>
-        );
-      }
-      if (value.data?.code === "NOT_FOUND") {
-        return (
-          <>
-            <h1>An error happened</h1>, We couldn't find a test with this id,
-            make sure you entered a valid test id
-            <p>{value.message}</p>
-          </>
-        );
-      }
-    },
-  });
+  const [testStarted, setTestStarted] = useState(false);
+  console.log(testStarted);
 
-  useEffect(() => {
-    const sessionId = localStorage.getItem("session_id");
-    console.log(sessionId);
-    if (!sessionId) {
-      sessionStarter.mutate({ test_id: test_id });
-    }
-    timer.mutate({ sessionId: sessionId! });
-    testData.mutate({ test_id: test_id });
-  }, []);
-
-  function handleTestStart() {
-    const sessionId = localStorage.getItem("session_id");
-    console.log(sessionId);
-    if (!test_id) {
-      return;
-    }
-    if (!sessionId) {
-      sessionStarter.mutate({ test_id: test_id });
-    }
-    timer.mutate({ sessionId: sessionId! });
+  function HandleLeaveTest() {
+    setTestStarted(false);
+    console.log("hi");
+    return;
   }
 
-  if (!testData.data?.testData) {
-    return <>Loading...</>;
+  function HandleTestStart() {
+    setTestStarted(true);
+    console.log("hiii");
+    return;
+    if (!sessionId) {
+      sessionStarter.mutate({ test_id: test_id! });
+    }
   }
-
   return (
     <>
-      <div className="flex h-screen flex-col items-center border border-black bg-[#EDF0FF]">
-        <TestTopBar testData={testData.data?.testData} />
-        <button
-          className="rounded bg-white p-3 font-bold text-black"
-          onClick={() => {
-            handleTestStart();
-          }}
-        >
-          Start Test
-        </button>
+      <div
+        className={`flex h-full w-full justify-center ${
+          testStarted ? "" : "items-center "
+        }`}
+      >
+        <TestStartModal
+          test_id={test_id}
+          testStarted={testStarted}
+          startOnClick={HandleTestStart}
+          leaveOnClick={HandleLeaveTest}
+        />
       </div>
     </>
   );
 }
 
-export function TestTopBar(props: {
-  testData: { title: string; description: string | null; date: Date | null };
-  timeSession?: number;
+export function TestStartModal(props: {
+  test_id: string;
+  testStarted: boolean;
+  startOnClick: () => void;
+  leaveOnClick: () => void;
 }) {
+  const { isLoading, isError, data, error } =
+    api.tests.getTestIntroWithId.useQuery({
+      test_id: props.test_id,
+    });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <Error message={error.message} code={error.data?.code} />;
+  }
   return (
-    <div className="w-[100%] max-w-2xl ">
-      <div className="flex flex-row items-center justify-between gap-2 bg-[#FBFBFF] p-4 shadow-lg">
-        <div>
-          <h1 className="text-lg font-bold text-[#1A2643]">
-            {props.testData.title}
-          </h1>
-          <p className="text-sm">{props.testData.description}</p>
+    <div className="w-full max-w-2xl">
+      <div
+        className={`flex flex-col gap-y-3 ${
+          props.testStarted ? "" : "rounded-lg"
+        } bg-[#FBFBFF] p-4 shadow-lg`}
+      >
+        <div className="flex flex-row items-center justify-between gap-2 ">
+          <div className="flex flex-col">
+            <h1 className="text-lg font-bold text-[#1A2643]">
+              {data.testData?.title}{" "}
+            </h1>
+            <p className="text-sm">{data.testData?.description}</p>
+          </div>
+          <div>
+            <p className="font-light">
+              Published on: {data.testData?.publishDate?.toDateString()}
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="font-light">
-            Published on: {props.testData.publishedAt?.toDateString()}
-          </p>
-        </div>
+        {props.testStarted ? null : (
+          <button
+            className="rounded border border-black px-2 py-1 text-[#1A2643] transition-colors hover:bg-[#1A2643] hover:text-white"
+            onClick={() => {
+              props.startOnClick();
+            }}
+          >
+            Start Test
+          </button>
+        )}
       </div>
-      <div className="flex justify-between rounded-b-lg bg-[#1A2643] px-3 py-1 text-white">
-        <p className="font-semibold">Leave test</p>
-        <p className="font-light">
-          Time Remaining: {props.timeSession ? props.timeSession : 0}
-        </p>
+      <div className="flex justify-end rounded-b-lg bg-[#1A2643] px-3 py-1 text-white">
+        {props.testStarted ? (
+          <TestControls leaveHandler={props.leaveOnClick} />
+        ) : null}
       </div>
     </div>
   );
+}
+
+export function TestControls(props: { leaveHandler: () => void }) {
+  const timer = useTimer();
+  return (
+    <div className="flex w-full flex-row justify-between">
+      <div
+        onClick={() => {
+          props.leaveHandler();
+        }}
+      >
+        Leave Test
+      </div>
+      <div>
+        <span className="font-light">Time Remaining: {timer}</span>
+      </div>
+    </div>
+  );
+}
+
+export function useTimer() {
+  const [timer, setTimer] = useState("0");
+  return timer;
+}
+
+export function StartTestButton(props: { onClick: () => void }) {
+  return <>hi</>;
+}
+
+export function Test(props: { testId: string }) {
+  const { isLoading, isError, data, error } = api.tests.getTestWithId.useQuery({
+    test_id: props.testId!,
+  });
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (isError) {
+    return <Error message={error.message} code={error.data?.code} />;
+  }
+  return (
+    <div className="">
+      <Question question={data.testData!} />
+    </div>
+  );
+}
+
+export function Question(props: { question: Question }) {
+  return <div></div>;
+}
+
+export function Error(props: { code?: string; message: string }) {
+  return (
+    <>
+      <div className="w-full p-2">
+        <h1 className="text-lg font-bold">An error occured,</h1> {props.message}
+        <p>
+          {props.code ? (
+            <>
+              <h2>Code</h2>
+              props.code
+            </>
+          ) : null}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export function Loading() {
+  return <>Loading...</>;
 }
--- quickthink/src/server/api/routers/testRouter.ts
+++ quickthink/src/server/api/routers/testRouter.ts
@@ -3,7 +3,13 @@ import { create } from "domain";
 import { and, eq } from "drizzle-orm";
 import { Input } from "postcss";
 import { z } from "zod";
-import { sessions, tests } from "~/drizzle/schema";
+import {
+  Question,
+  answers,
+  questions,
+  sessions,
+  tests,
+} from "~/drizzle/schema";
 import {
   authenticatedProcedure,
   createTRPCRouter,
@@ -11,23 +17,48 @@ import {
 } from "~/server/api/trpc";
 
 export const testRouter = createTRPCRouter({
-  getTestWithId: publicProcedure
+  getTestDataWithId: publicProcedure
     .input(z.object({ test_id: z.string().uuid() }))
-    .mutation(async ({ ctx, input }) => {
+    .query(async ({ ctx, input }) => {
       const db = ctx.db;
-      const test = await db
-        .select()
-        .from(tests)
-        .where(eq(tests.id, input.test_id));
 
-      if (test.length > 0) {
-        return { testData: test[0] };
-      }
-      throw new TRPCError({ code: "NOT_FOUND" });
+      const testQuestions = await db
+        .select()
+        .from(questions)
+        .where(eq(questions.testId, input.test_id));
+
+      const getTestAnswers = async (questions: Question[]) => {
+        questions.forEach((question) => {});
+      };
+    }),
+  getTestQuestions: publicProcedure
+    .input(z.object({ test_id: z.string().uuid() }))
+    .query(async ({ ctx, input }) => {
+      const db = ctx.db;
+      const testQuestions = await db
+        .select()
+        .from(questions)
+        .where(eq(questions.testId, input.test_id));
+      return { questions: testQuestions };
+    }),
+  getQuestionAnswers: publicProcedure
+    .input(z.object({ question_id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const db = ctx.db;
+      const questionAnswers = await db
+        .select({
+          id: answers.id,
+          questionId: answers.questionId,
+          content: answers.content,
+        })
+        .from(answers)
+        .where(eq(answers.questionId, input.question_id));
+
+      return { answers: questionAnswers };
     }),
   getTestIntroWithId: publicProcedure
     .input(z.object({ test_id: z.string().uuid() }))
-    .mutation(async ({ ctx, input }) => {
+    .query(async ({ ctx, input }) => {
       const db = ctx.db;
       const test = await db
         .select({
@@ -60,7 +91,7 @@ export const testRouter = createTRPCRouter({
         sessionId: z.string().uuid(),
       }),
     )
-    .mutation(async ({ ctx, input }) => {
+    .query(async ({ ctx, input }) => {
       const db = ctx.db;
       const session = await db
         .select({
@@ -74,15 +105,37 @@ export const testRouter = createTRPCRouter({
       if (session.length === 0) {
         throw new TRPCError({ code: "NOT_FOUND" });
       }
-      const maxTestTime =
-        session[0]?.startTime!.getTime()! + Number(session[0]?.testTimeLength);
-      if (Date.now() > maxTestTime) {
+      const maxTestTime = new Date(
+        session[0]?.startTime!.getTime()! +
+          Number(session[0]?.testTimeLength) * 1000,
+      );
+      if (new Date() > maxTestTime) {
         throw new TRPCError({
-          code: "BAD_REQUEST",
+          code: "FORBIDDEN",
           message: "Your session has run out.",
         });
       }
-
-      return session;
+      const remainingTime = calculateRemainingTime(maxTestTime);
+      console.log(remainingTime);
+      return { session: session, timer: remainingTime };
     }),
 });
+
+function calculateRemainingTime(endTime: Date) {
+  const currentTime = new Date();
+  const timeDifference = endTime.getTime() - currentTime.getTime();
+
+  const seconds = Math.floor((timeDifference / 1000) % 60);
+  const minutes = Math.floor((timeDifference / 1000 / 60) % 60);
+  const hours = Math.floor((timeDifference / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  const remainingTime = {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+
+  return remainingTime;
+}
