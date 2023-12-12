e52a35e70e132ed9f26ab87f84b2fc52ad24c5bb
zia-zedia <ziagallaf578@gmail.com>
2023-12-12 15:21:30 +0300

    trying to deploy 4.o 	modified:   ../.gitignore 	modified:   ../next.config.mjs

--- quickthink/next.config.mjs
+++ quickthink/next.config.mjs
@@ -9,15 +9,6 @@ const config = {
   reactStrictMode: false,
   distDir: "dist",
   output: "export",
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
