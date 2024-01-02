1421584438c54b10f1d69ec1ade151ef971bad2a
zia-zedia <ziagallaf578@gmail.com>
2024-01-02 22:12:39 +0300

    something

--- quickthink/src/components/Navbar.tsx
+++ quickthink/src/components/Navbar.tsx
@@ -1,19 +1,24 @@
+import Link from "next/link";
 import { ReactNode, useState } from "react";
 
 export function Navbar(props: { children?: ReactNode }) {
   return (
     <div className="sticky h-full w-[10%] max-w-[70px] bg-[#1A2643] p-3 text-white">
-      <div className="flex justify-between">
-        <img
-          src={"/white_logo.png"}
-          alt="QuickThink Logo"
-          className="w-full object-scale-down p-1"
-        />
+      <div className="flex flex-col justify-between">
+        <div className="">
+          <Link href={"/"}>
+            <img
+              src={"/white_logo.png"}
+              alt="QuickThink Logo"
+              className="w-full object-scale-down p-1"
+            />
+          </Link>
+          <div className="flex flex-col gap-3">
+            {props.children ? props.children : null}
+          </div>
+        </div>
+        <div>Logout</div>
       </div>
-      <div className="flex flex-col gap-3">
-        {props.children ? props.children : null}
-      </div>
-      <div>Logout</div>
     </div>
   );
 }
